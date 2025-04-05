fe4201255a8f0f4f026efcab8d0e57be76186623
yronnede <101017151+yronnede@users.noreply.github.com>
2025-04-05 21:47:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(function (req, res, next) {
 });
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-//app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
+app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 100, headers: true }));
 
 app.all('/player/login/dashboard', function (req, res) {
     const tData = {};
